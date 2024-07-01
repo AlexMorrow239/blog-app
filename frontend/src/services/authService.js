bd8461857b4f51931fc85eb42dd4f0c06e71debc
AlexMorrow239 <alex.morrow239@gmail.com>
2024-07-01 15:09:39 +0300

    fixed managing dev env and prod env

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,4 +1,4 @@
-import { BASE_API_URL } from "../config";
+const BASE_API_URL = process.env.REACT_APP_API_URL;
 
 const register = async (userData) => {
   const response = await fetch(`${BASE_API_URL}/auth/register`, {
