09c5517bf5adc1874413e7a5255173f9ae3ff5bd
AlexMorrow239 <alex.morrow239@gmail.com>
2024-07-05 13:10:25 +0300

    feat: Add Navbar component to Login page

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -5,6 +5,7 @@ import { login, resetSuccessAndError } from "../../features/authSlice";
 
 import SuccessToast from "../../components/SuccessToast";
 import ErrorToast from "../../components/ErrorToast";
+import Navbar from "../../components/Navbar";
 
 import "./index.css";
 
@@ -47,11 +48,12 @@ export default function LoginPage() {
 
   return (
     <>
+      <Navbar />
       <div className="html-body">
         <main className="form-signin">
           <form onSubmit={onSubmit}>
             <h1 className="h3 mb-3 fw-normal">Please login</h1>
-            <div className="form-floating">
+            <div className="form-floating mb-2">
               <input
                 type="email"
                 className="form-control"
@@ -63,7 +65,7 @@ export default function LoginPage() {
               />
               <label htmlFor="floatingInput">Email address</label>
             </div>
-            <div className="form-floating">
+            <div className="form-floating mb-2">
               <input
                 type="password"
                 className="form-control"
